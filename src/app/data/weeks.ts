4e5e4655148a803c33a422397fd71ac1e1f15cc8
Mike Spensieri <mikespensieri@smooch.io>
2025-09-27 08:13:54 -0400

    Week 1

--- src/app/data/weeks.ts
+++ src/app/data/weeks.ts
@@ -21,4 +21,153 @@ interface Week extends Partial<Record<PlayerKeys, Points>> {
   jury?: Array<PlayerKeys>;
 }
 
-export const weeks: Array<Week> = [{}];
+const {
+  ALEX,
+  ANNIE,
+  JAKE,
+  JASON,
+  JAWAN,
+  JEREMIAH,
+  KRISTINA,
+  MATT,
+  MC,
+  NATE,
+  NICOLE,
+  RIZO,
+  SAGE,
+  SAVANNAH,
+  SHANNON,
+  SOPHI,
+  SOPHIE,
+  STEVEN,
+} = PlayerKeys;
+
+export const weeks: Array<Week> = [
+  {
+    [ALEX]: {
+      votes: 1,
+      upsideDown: [
+        {
+          reason: "Losing team",
+          points: 2,
+        },
+        {
+          reason: "Abysmal challenge",
+          points: 2,
+        },
+      ],
+    },
+    [ANNIE]: {
+      votes: 1,
+      upsideDown: [
+        {
+          reason: "Losing team",
+          points: 2,
+        },
+      ],
+    },
+    [JAKE]: {
+      votes: 1,
+      upsideDown: [
+        {
+          reason: "Losing team",
+          points: 2,
+        },
+      ],
+    },
+    [JASON]: {
+      teamImmunity: 1,
+    },
+    [JAWAN]: {
+      teamImmunity: 1,
+      upsideDown: [
+        {
+          reason: "Random crying",
+          points: 4,
+        },
+      ],
+    },
+    [JEREMIAH]: {
+      votes: 1,
+      upsideDown: [
+        {
+          reason: "Losing team",
+          points: 2,
+        },
+      ],
+    },
+    [KRISTINA]: {
+      teamImmunity: 1,
+    },
+    [MATT]: {
+      teamImmunity: 1,
+    },
+    [MC]: {
+      teamImmunity: 1,
+    },
+    [NATE]: {
+      teamImmunity: 1,
+    },
+    [NICOLE]: {
+      upsideDown: [
+        {
+          reason: "Losing team",
+          points: 2,
+        },
+        {
+          reason: "Vomit",
+          points: 2,
+        },
+        {
+          reason: "Voted wrong",
+          points: 2,
+        },
+        {
+          reason: "Blindsided at tribal",
+          points: 6,
+        },
+        {
+          reason: "Voted off first",
+          points: 6,
+        },
+      ],
+    },
+    [RIZO]: {
+      teamImmunity: 1,
+      upsideDown: [
+        {
+          reason: "Abysmal challenge",
+          points: 2,
+        },
+        {
+          reason: "Random crying",
+          points: 4,
+        },
+      ],
+    },
+    [SAGE]: {
+      teamImmunity: 1,
+    },
+    [SAVANNAH]: {
+      teamImmunity: 1,
+    },
+    [SHANNON]: {
+      teamImmunity: 1,
+    },
+    [SOPHI]: {
+      votes: 1,
+      upsideDown: [
+        {
+          reason: "Losing team",
+          points: 2,
+        },
+      ],
+    },
+    [SOPHIE]: {
+      teamImmunity: 1,
+    },
+    [STEVEN]: {
+      teamImmunity: 1,
+    },
+  },
+];
